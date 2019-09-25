7715b8b3a0a294769bd322f86771526698ef1da6
ariqzuhdi <ariqzuhdi1@outlook.com>
2019-09-25 17:45:07 +0700

    add bandingkan-angka.js

---
+++ Jum'at/Bandingkan-angka.js
@@ -0,0 +1,17 @@
+function bandingkanAngka(angka1, angka2){
+    if(angka1 < angka2){
+        console.log(true)
+    }
+    else if(angka1 > angka2){
+        console.log(false)
+    }
+    else if(angka1 == angka2){
+        console.log(-1)
+    }
+}
+
+console.log(bandingkanAngka(5, 8));
+console.log(bandingkanAngka(5, 3));
+console.log(bandingkanAngka(4, 4));
+console.log(bandingkanAngka(3, 3));
+console.log(bandingkanAngka(17, 2));
